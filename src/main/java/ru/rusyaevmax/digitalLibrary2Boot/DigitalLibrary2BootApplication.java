81220101ab3f9e8f7ba09041e539714a2912d944
Razbuer <Rusyaevmax@yandex.ru>
2022-11-22 01:07:26 +0300

    Initial commit

---
+++ src/main/java/ru/rusyaevmax/digitalLibrary2Boot/DigitalLibrary2BootApplication.java
@@ -0,0 +1,13 @@
+package ru.rusyaevmax.digitalLibrary2Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLibrary2BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalLibrary2BootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rusyaevmax/digitalLibrary2Boot/DigitalLibrary2BootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rusyaevmax.digitalLibrary2Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalLibrary2BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
